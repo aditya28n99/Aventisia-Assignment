72301b5a4b2362b916bb2920553e33814a7cd6cd
aditya28n99 <adityawakale208@gmail.com>
2025-01-09 02:30:48 +0530

    remaining components added

--- src/pages/ApplicationLandingScreen.jsx
+++ src/pages/ApplicationLandingScreen.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import Sidebar from '../components/Navbar/Sidebar';
 import ModelLibrary from '../components/ModelLibrary/ModelLibrary';
 import LabelData from '../components/LabelData/LabelData';
+import Model from '../components/Model/Model';
+import Test from '../components/Test/Test';
+import Settings from '../components/Settings/Settings';
+import Support from '../components/Support/Support';
 
 export default function ApplicationLandingScreen() {
   //setting up a component state for the screen from sidebar
@@ -14,7 +18,16 @@ export default function ApplicationLandingScreen() {
         return <ModelLibrary />;
       case 'LabelData':
         return <LabelData />;
-      // will add others if works fine..
+      case 'Model':
+        return <Model />;
+      case 'Test':
+        return <Test />;
+      case 'Settings':
+        return <Settings />;
+      case 'Support':
+        return <Support />;
+      default:
+        return <ModelLibrary />;
     }
   };
 
