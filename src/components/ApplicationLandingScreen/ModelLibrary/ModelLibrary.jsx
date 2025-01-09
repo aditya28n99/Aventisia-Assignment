d27dcd957f5de2d8a3e6f84d2e3d1d931e4b68e2
aditya28n99 <adityawakale208@gmail.com>
2025-01-09 14:33:52 +0530

    lib table added with dummy data

--- src/components/ApplicationLandingScreen/ModelLibrary/ModelLibrary.jsx
+++ src/components/ApplicationLandingScreen/ModelLibrary/ModelLibrary.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 import { PiArrowsLeftRightLight } from "react-icons/pi";
+import { BiSortAlt2 } from "react-icons/bi";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import DateRangePicker from "../../ModelLibrary/DateRangePicker/DateRangePicker";
 
 export default function ModelLibrary() {
 
+    // Dummy data for the 
+    const data = [
+        {
+            id: "#5412448",
+            modelName: "Blonde Drizzle",
+            modelType: "Extraction",
+            description: "Edit customer and there truncate max-w-xs truncate max-w-xs truncate max-w-xs",
+            createdOn: "29/02/2024",
+            lastTrainedOn: "02/03/2024",
+            status: "Active",
+        },
+        {
+            id: "#5412448",
+            modelName: "John Sina",
+            modelType: "Classification",
+            description: "Edit customer",
+            createdOn: "29/02/2024",
+            lastTrainedOn: "02/03/2024",
+            status: "Expired",
+        },
+    ];
+
     return (
-        <div className="p-6">
+        <div className="p-5 border rounded-md">
             {/*Top Section for model for searching sorting*/}
             <div className="flex flex-col gap-3">
                 <div className="flex justify-between items-center">
@@ -34,7 +58,73 @@ export default function ModelLibrary() {
                             <option value="desc" className="p-2">Z-A</option>
                         </select>
                     </div>
-                    <DateRangePicker/>
+                    <DateRangePicker />
+                </div>
+                {/* Mid Section (Table) */}
+                <div className="rounded-lg bg-white">
+                    <table className="w-full border-collapse">
+                        <thead className="text-left border-b">
+                            <tr>
+                                <th className="p-4 font-semibold">
+                                    Model Name <BiSortAlt2 className="inline ml-2 cursor-pointer" />
+                                </th>
+                                <th className="p-4 font-semibold">
+                                    Model Type <BiSortAlt2 className="inline ml-2 cursor-pointer" />
+                                </th>
+                                <th className="p-4 font-semibold">
+                                    Description <BiSortAlt2 className="inline ml-2 cursor-pointer" />
+                                </th>
+                                <th className="p-4 font-semibold">
+                                    Created On <BiSortAlt2 className="inline ml-2 cursor-pointer" />
+                                </th>
+                                <th className="p-4 font-semibold">
+                                    Last Trained On <BiSortAlt2 className="inline ml-2 cursor-pointer" />
+                                </th>
+                                <th className="p-4 font-semibold">
+                                    Status <BiSortAlt2 className="inline ml-2 cursor-pointer" />
+                                </th>
+                                <th className="p-4 font-semibold">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((item, index) => (
+                                <tr
+                                    key={index}
+                                    className="border-b hover:bg-gray-100"
+                                >
+                                    <td className="p-4">
+                                        <div>
+                                            <div className="font-semibold">{item.modelName}</div>
+                                            <div className="text-sm text-gray-500">ID: {item.id}</div>
+                                        </div>
+                                    </td>
+                                    <td className="p-4">{item.modelType}</td>
+                                    {/* added trucate not to wrap the line */}
+                                    <td className="p-4 truncate max-w-xs">{item.description}</td>
+                                    <td className="p-4 text-center">{item.createdOn}</td>
+                                    <td className="p-4 text-center">{item.lastTrainedOn}</td>
+                                    <td className="p-4 text-center">
+                                        <span className="bg-green-100 text-green-600 px-2 py-1 rounded-lg">
+                                            {item.status}
+                                        </span>
+                                    </td>
+                                    <td className="p-4 text-center">
+                                        <button className="relative">
+                                            <BsThreeDotsVertical />
+                                            <div className="absolute right-0 mt-2 w-32 bg-white shadow-lg rounded-lg p-2 hidden group-hover:block">
+                                                <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+                                                    Edit
+                                                </button>
+                                                <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+                                                    Delete
+                                                </button>
+                                            </div>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
