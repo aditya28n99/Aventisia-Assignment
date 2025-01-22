0e2906d2376690f90e765ffe970254c7c1123072
aditya28n99 <adityawakale208@gmail.com>
2025-01-22 18:00:14 +0530

    FC added to the Dashbord folder

---
+++ src/components/Dashboard/LabelData/LabelData.tsx
@@ -0,0 +1,6 @@
+const LabelData: React.FC = () => {
+    return <div>LabelData Content</div>;
+  };
+  
+  export default LabelData;
+  
---
+++ src/components/Dashboard/ModelLibrary/ModelLibrary.tsx
@@ -0,0 +1,6 @@
+const ModelLibrary: React.FC = () => {
+    return <div>Model Library Content</div>;
+  };
+  
+  export default ModelLibrary;
+  
