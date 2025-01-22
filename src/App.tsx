48adad62cc56b511471e3b1123a3ac1db92ff8d5
aditya28n99 <adityawakale208@gmail.com>
2025-01-22 17:20:45 +0530

    installed shadcn and updated configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
 import React from 'react';
+import { Button } from "@/components/ui/button"
+
 
 const App: React.FC = () => {
 
   return (
-      <>
-      <h1>Hello React!!</h1>
-      </>  
+    <>
+      <h1 className='text-red-500'>Hello React!!</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
   )
 }
 
