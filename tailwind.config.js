c81f4e928f2af093b758b66450ed453d3a710bf9
aditya28n99 <adityawakale208@gmail.com>
2025-01-22 18:09:47 +0530

    added Aptos font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 export default {
     darkMode: ["class"],
     content: [
@@ -53,7 +55,10 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
+  		},
+		fontFamily: {
+		sans: ['Aptos', ...fontFamily.sans],
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
