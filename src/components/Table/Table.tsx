ad2aa155532a92f407bb0c3a3c1f6056ae3e0d02
aditya28n99 <adityawakale208@gmail.com>
2025-01-23 03:24:26 +0530

    added table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,63 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { BiSortAlt2 } from "react-icons/bi";
+
+import { initialData } from '../../Database/data';
+
+const TableComponent: React.FC = () => {
+    return (
+        <Table className="text-[16px]">
+            <TableHeader>
+                <TableRow>
+                    <TableHead>
+                        <div className="flex items-center">
+                            Model Name <BiSortAlt2 className="ml-2" />
+                        </div>
+                    </TableHead>
+                    <TableHead><div className="flex items-center">
+                        Model Type <BiSortAlt2 className="ml-2" />
+                    </div></TableHead>
+                    <TableHead><div className="flex items-center">
+                        Description <BiSortAlt2 className="ml-2" />
+                    </div></TableHead>
+                    <TableHead><div className="flex items-center">
+                        Created On <BiSortAlt2 className="ml-2" />
+                    </div></TableHead>
+                    <TableHead><div className="flex items-center">
+                        Last Trained On <BiSortAlt2 className="ml-2" />
+                    </div></TableHead>
+                    <TableHead><div className="flex items-center">
+                        Status <BiSortAlt2 className="ml-2" />
+                    </div></TableHead>
+                    <TableHead><div className="flex items-center">
+                        Action
+                    </div></TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {initialData.map((invoice) => (
+                    <TableRow key={invoice.id}>
+                        <TableCell className="font-medium py-[12.5px]">{invoice.modelName}</TableCell>
+                        <TableCell>{invoice.modelType}</TableCell>
+                        <TableCell className="truncate max-w-xs">{invoice.description}</TableCell>
+                        <TableCell className="text-center">{invoice.createdOn}</TableCell>
+                        <TableCell className="text-center">{invoice.lastTrainedOn}</TableCell>
+                        <TableCell className="text-center"><span className={`${invoice.status === 'Active' ? "bg-green-100 text-green-600" : "bg-red-100 text-red-600"} px-4 py-1 rounded-lg text-sm`}>
+                            {invoice.status}
+                        </span></TableCell>
+                        <TableCell className="flex justify-center"><BsThreeDotsVertical /></TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
+
+export default TableComponent;
