e03b9f32e6ab29d94723795efc292d743a372815
aditya28n99 <adityawakale208@gmail.com>
2025-01-23 03:26:34 +0530

    updated model library with components

--- src/components/Dashboard/ModelLibrary/ModelLibrary.tsx
+++ src/components/Dashboard/ModelLibrary/ModelLibrary.tsx
@@ -1,6 +1,58 @@
+import React from "react";
+import { PiArrowsLeftRightLight } from "react-icons/pi";
+import { FiPlus } from "react-icons/fi";
+import Button from "../../Dashboard/Navigations/Button";
+// import { initialData } from '../../../Database/data'
+import { Search } from 'lucide-react';
+import DateRangePicker from "@/components/DateRangePicker/DateRangePicker";
+import PaginationComponent from "@/components/Pagination/Pagination";
+import TableComponent from "@/components/Table/Table";
+
+
 const ModelLibrary: React.FC = () => {
-    return <div>Model Library Content</div>;
-  };
-  
-  export default ModelLibrary;
-  
+  return (
+    <div className="p-5 pt-0 rounded-md bg-white h-[740px]" >
+      {/*Top Section for model for searching sorting*/}
+      <div className="flex flex-col gap-3 bg-white pt-5 z-10">
+        <div className="flex justify-between items-center">
+          <h1 className="text-lg font-medium">Model Library</h1>
+          {/* Importing Button for create new model. */}
+          <Button
+            onClick={() => { }}
+            className="bg-[#4F46E5] text-white px-4 py-2 font-semibold text-base rounded-xl"
+            icon={FiPlus}>
+            Create New Model
+          </Button>
+        </div>
+        {/* New line - serch, filter, and sorting */}
+        <div className="flex items-center gap-4 text-[#767676]">
+          <div className="flex items-center bg-[#F9FAFB] px-4 py-2 rounded-lg w-full">
+            <Search className="text-black" />
+            <input
+              type="text"
+              placeholder="Search By Name, ID"
+              className="bg-transparent outline-none pl-2"
+              onChange={() => { }}
+            />
+          </div>
+          {/* Here we can filter out data by A-Z or Z-A */}
+          <button className="flex items-center gap-2 border rounded-lg px-4 py-2">
+            <PiArrowsLeftRightLight className="text-gray-500" />
+            <h1 className="font-extralight">Filter</h1>
+          </button>
+          <DateRangePicker/>
+        </div>
+        {/* Mid Section (Table) */}
+        <div className="rounded-lg bg-white h-[488px] overflow-scroll">
+          <TableComponent/>
+        </div>
+        {/* Bottom Section (Pagination) */}
+        <div className="mt-[50px]">
+          <PaginationComponent />
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default ModelLibrary;
